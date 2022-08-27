446716aed1ddf4bf220172d1c5a0b136e5e67a97
Geremia Pompei <geremiapompei@gmail.com>
2022-08-27 02:38:35 +0200

    add translator

---
+++ translate/google_translator.py
@@ -0,0 +1,28 @@
+import requests
+
+def translate(input: str, lan_to: str, lan_from: str = 'auto') -> str:
+    """This function exploits google translate api to translate a sentence from a language to another.
+
+    Args:
+        input (str): Input sentence to translate,
+        lan_to (str): Label that encode the language symbol of target sentence.
+        lan_from (str, optional): Label that encode the language symbol of input sentence. Default value is "auto".
+
+    Returns:
+        str: Target sentence.
+    """
+    input = input.replace("\"", "\\\\\\\"", -1)
+    start_pattern = "[[\\\""
+    stop_pattern = "\\\","
+    res = requests.post(
+        "https://translate.google.it/_/TranslateWebserverUi/data/batchexecute?rpcids=MkEWBc&source-path=%2F&f.sid=6697384723060200247&bl=boq_translate-webserver_20220824.02_p0&hl=it&soc-app=1&soc-platform=1&soc-device=1&_reqid=2301709&rt=c",
+        headers={
+            "content-type": "application/x-www-form-urlencoded;charset=UTF-8"
+        },
+        data=f'f.req=[[["MkEWBc","[[\\"{input}\\",\\"{lan_from}\\",\\"{lan_to}\\",true],[null]]",null,"generic"]]]'
+    ).text
+    res = res[res.index(start_pattern) + len(start_pattern):]
+    res = res[res.index(start_pattern) + len(start_pattern):]
+    res = res[:res.index(stop_pattern)]
+    res = res.replace("\\\\\\\"", "\"", -1)
+    return res
