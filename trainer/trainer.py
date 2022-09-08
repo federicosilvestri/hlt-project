12410e2b4cf37454ea868fd904c872ade33d7e92
Geremia Pompei <geremiapompei@gmail.com>
2022-09-08 19:31:49 +0200

    add transformer files, trainer files and implementation and transformer_modek files and implamentation

---
+++ trainer/trainer.py
@@ -0,0 +1,111 @@
+import random
+import math
+import time
+from typing import Callable, List, Tuple
+import torch
+from torch import nn
+import logging as lg
+
+class Trainer:
+    """This class create the instance able to train a trnsformer model for a NMT task."""
+
+    def __init__(self, model: nn.Module, trg_pad_idx: int, learning_rate: float = 0.0005, batch_size: int = 64, clip: int = 1) -> None:
+        """Trainer constructor
+
+        Args:
+            model (nn.Module): Transformer model that will be trained.
+            trg_pad_idx (int): Index that represent padding tag on decoder tokenizer.
+            learning_rate (float, optional): Learning rate used from Adam optimizer.
+            batch_size (int): Batch size to create minibatches.
+            clip (int, optional): Parameter used to clip weights norm each minibatch training.
+        """
+        self.model = model
+        self.clip = clip
+        self.batch_size = batch_size
+        self.optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+        self.criterion = nn.CrossEntropyLoss(ignore_index = trg_pad_idx)
+
+    def __epoch_time(self, start_time: float, end_time: float) -> Tuple[int, int]:
+        """Method able to extract minutes and seconds from a start and an end time.
+
+        Args:
+            start_time (float): Start time of a counter.
+            end_time (float): End time of a counter.
+
+        Returns:
+            Tuple[int, int]: Duration in minutes and seconds.
+        """
+        elapsed_time = end_time - start_time
+        elapsed_mins = int(elapsed_time / 60)
+        elapsed_secs = int(elapsed_time - (elapsed_mins * 60))
+        return elapsed_mins, elapsed_secs
+
+    def __train_epoch(self, train_set: List[Tuple[torch.tensor, torch.tensor]]) -> float:
+        """Method used to train one single epoch.
+
+        Args:
+            train_set (List[Tuple[torch.tensor, torch.tensor]])): Train set.
+        
+        Return:
+            float: Loss related to the current epoch.
+        """
+        self.model.train()
+        epoch_loss = 0
+        for src, trg in train_set:
+            self.optimizer.zero_grad()
+            output, _ = self.model(src, trg[:,:-1])
+            #output = [batch size, trg len - 1, output dim]
+            #trg = [batch size, trg len]
+            output_dim = output.shape[-1]
+            output = output.contiguous().view(-1, output_dim)
+            trg = trg[:,1:].contiguous().view(-1)
+            #output = [batch size * trg len - 1, output dim]
+            #trg = [batch size * trg len - 1]
+            loss = self.criterion(output, trg)
+            loss.backward()
+            torch.nn.utils.clip_grad_norm_(self.model.parameters(), self.clip)
+            self.optimizer.step()
+            epoch_loss += loss.item()
+        return epoch_loss / len(train_set)
+
+    def __create_batches(self, train_set: List[Tuple[torch.tensor, torch.tensor]]) -> List[Tuple[torch.tensor, torch.tensor]]:
+        """Method able to create batches of a train set.
+
+        Args:
+            train_set (List[Tuple[torch.tensor, torch.tensor]]): Original training set.
+
+        Returns:
+            List[Tuple[torch.tensor, torch.tensor]]: Minibatch training set.
+        """
+        num_batches = math.ceil(len(train_set) / self.batch_size)
+        minibatch_tr = []
+        for num_batch in range(num_batches):
+            stop_index = (num_batch+1) * self.batch_size
+            stop_index = stop_index if len(train_set) > stop_index else len(train_set)
+            batch_train_data = train_set[num_batch * self.batch_size : stop_index]
+            src, trg = zip(*batch_train_data)
+            minibatch_tr.append((torch.cat(src, 0), torch.cat(trg, 0)))
+        return minibatch_tr
+
+    def __call__(self, train_set: List[Tuple[torch.tensor, torch.tensor]], epochs: int = 10, callbacks: List[Callable]=[], verbose: bool = True) -> None:
+        """Main method of the class able to train the model.
+
+        Args:
+            train_set (List[Tuple[torch.tensor, torch.tensor]])): Train set.
+            epochs (int, optional): Number of time that the train process is repeated.
+            callbacks (List[Callable], optional): List of callbacks called after each epoch.
+            verbose (bool, optional): Flag that is used to decide to print or not training results on console.
+        """
+        lg.info("Start training")
+        for epoch in range(epochs):
+            start_time = time.time()
+            random.shuffle(train_set)
+            mb_train_set = self.__create_batches(train_set)
+            train_loss = self.__train_epoch(mb_train_set)
+            end_time = time.time()
+            epoch_mins, epoch_secs = self.__epoch_time(start_time, end_time)
+            if verbose:
+                print(f'Epoch: {epoch+1:02} | Time: {epoch_mins}m {epoch_secs}s')
+                print(f'\tLoss: {math.exp(train_loss):7.3f}')
+            for callback in callbacks:
+                callback(train_loss, (epoch_mins, epoch_secs))
---
+++ translate/transformer_translator.py
@@ -0,0 +1,54 @@
+import torch
+import torch.nn
+from transformers import AutoTokenizer
+
+
+class TransformerTranslator:
+    """Class able to construct object that translate sentences given a transformer model."""
+
+    def __init__(self, model: nn.Module, tokenizer_encoder: AutoTokenizer, tokenizer_decoder: AutoTokenizer, max_length: int = 100, device: str = 'cpu') -> None:
+        """TransformerTranslator constructor.
+        
+        Args:
+            model (nn.Module): Model trained that is able to produce a word given a source cotext and a target sentence.
+            tokenizer_encoder (AutoTokenizer): Encoder tokenizer.
+            tokenizer_decoder (AutoTokenizer): Decoder tokenizer.
+            max_length (int, optional): Max number of tokens of a translated sentence.
+            device (str, optional): Accelerator type. Default 'cpu'.
+        """
+        self.model = model
+        self.tokenizer_encoder = tokenizer_encoder
+        self.tokenizer_decoder = tokenizer_decoder
+        self.max_length = max_length
+        self.device = device
+
+    def __call__(self, src_sentence: str) -> str:
+        """Method able to translate a given sentence.
+        
+        Args:
+            src_sentence (src): Source sentence.
+        
+        Returns:
+            str: Translated sentence.
+        """
+        src_indexes = self.tokenizer_encoder(src_sentence, return_tensors='pt', add_special_tokens=True,
+                                        max_length=self.max_length, padding='max_length',
+                                        truncation=True).to(self.device).data["input_ids"]
+        src_tensor = src_indexes.unsqueeze(0).to(self.device)
+        src_mask = self.model.make_src_mask(src_tensor)
+        with torch.no_grad():
+            src_mask = src_mask.squeeze(1).squeeze(2)
+            enc_src = self.model.encoder(src_tensor, src_mask)
+        trg_indexes = self.tokenizer_decoder.convert_tokens_to_ids(['[CLS]'])
+        for i in range(self.max_length):
+            trg_tensor = torch.tensor(trg_indexes).unsqueeze(0).to(self.device)
+            trg_mask = self.model.make_trg_mask(trg_tensor)
+            with torch.no_grad():
+                output, attention = self.model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+            pred_token = output.argmax(2)[:, -1].item()
+            trg_indexes.append(pred_token)
+            trg_tokens = self.tokenizer_decoder.convert_ids_to_tokens(trg_indexes)
+            if trg_tokens[-1] == '[SEP]':
+                break
+        res_sent = self.tokenizer_decoder.convert_tokens_to_string(trg_tokens[1:-1])
+        return res_sent
