82d2817b5070f72115701efe0d64307069c8479a
Geremia Pompei <geremiapompei@gmail.com>
2022-09-08 20:15:50 +0200

    Added bleu score on translator

--- translate/transformer_translator.py
+++ translate/transformer_translator.py
@@ -1,6 +1,8 @@
+from typing import List, Tuple
 import torch
 import torch.nn
 from transformers import AutoTokenizer
+from torchtext.data.metrics import bleu_score
 
 
 class TransformerTranslator:
@@ -8,7 +10,7 @@ class TransformerTranslator:
 
     def __init__(self, model: nn.Module, tokenizer_encoder: AutoTokenizer, tokenizer_decoder: AutoTokenizer, max_length: int = 100, device: str = 'cpu') -> None:
         """TransformerTranslator constructor.
-        
+
         Args:
             model (nn.Module): Model trained that is able to produce a word given a source cotext and a target sentence.
             tokenizer_encoder (AutoTokenizer): Encoder tokenizer.
@@ -24,16 +26,16 @@ class TransformerTranslator:
 
     def __call__(self, src_sentence: str) -> str:
         """Method able to translate a given sentence.
-        
+
         Args:
             src_sentence (src): Source sentence.
-        
+
         Returns:
             str: Translated sentence.
         """
         src_indexes = self.tokenizer_encoder(src_sentence, return_tensors='pt', add_special_tokens=True,
-                                        max_length=self.max_length, padding='max_length',
-                                        truncation=True).to(self.device).data["input_ids"]
+                                             max_length=self.max_length, padding='max_length',
+                                             truncation=True).to(self.device).data["input_ids"]
         src_tensor = src_indexes.unsqueeze(0).to(self.device)
         src_mask = self.model.make_src_mask(src_tensor)
         with torch.no_grad():
@@ -44,11 +46,33 @@ class TransformerTranslator:
             trg_tensor = torch.tensor(trg_indexes).unsqueeze(0).to(self.device)
             trg_mask = self.model.make_trg_mask(trg_tensor)
             with torch.no_grad():
-                output, attention = self.model.decoder(trg_tensor, enc_src, trg_mask, src_mask)
+                output, attention = self.model.decoder(
+                    trg_tensor, enc_src, trg_mask, src_mask)
             pred_token = output.argmax(2)[:, -1].item()
             trg_indexes.append(pred_token)
-            trg_tokens = self.tokenizer_decoder.convert_ids_to_tokens(trg_indexes)
+            trg_tokens = self.tokenizer_decoder.convert_ids_to_tokens(
+                trg_indexes)
             if trg_tokens[-1] == '[SEP]':
                 break
-        res_sent = self.tokenizer_decoder.convert_tokens_to_string(trg_tokens[1:-1])
+        trg_tokens = trg_tokens[1:-1]
+        res_sent = self.tokenizer_decoder.convert_tokens_to_string(trg_tokens)
         return res_sent
+
+    def bleu(self, test_set: List[Tuple[str, str]]) -> float:
+        """Method used to compute bleu score.
+
+        Args: 
+            test_set (List[Tuple[torch.tensor, torch.tensor]]): Test set.
+
+        Returns:
+            float: BLEU score.
+        """
+        trgs = []
+        pred_trgs = []
+        for src, trg in test_set:
+            trg_tokens = self.tokenizer_decoder.tokenize(trg)
+            trgs.append([trg_tokens])
+            pred_trg = self.__call__(src)
+            pred_trg_tokens = self.tokenizer_decoder.tokenize(pred_trg)
+            pred_trgs.append(pred_trg_tokens)
+        return bleu_score(pred_trgs, trgs)*100
