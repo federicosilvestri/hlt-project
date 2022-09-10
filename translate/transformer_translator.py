38006a6d9df699bea5a94f67d85be6d7d073358c
Geremia Pompei <geremiapompei@gmail.com>
2022-09-10 18:46:37 +0200

    bug fix typing

--- translate/transformer_translator.py
+++ translate/transformer_translator.py
@@ -1,4 +1,4 @@
-from typing import List
+from typing import List, Tuple
 import torch
 from torch import nn
 from torchtext.data.metrics import bleu_score
@@ -76,7 +76,7 @@ class TransformerTranslator:
         res_sent = self.tokenizer_decoder.convert_tokens_to_string(trg_tokens)
         return res_sent
 
-    def bleu(self, test_set: List[tuple[str, str]]) -> float:
+    def bleu(self, test_set: List[Tuple[str, str]]) -> float:
         """Method used to compute bleu score.
 
         Args:
