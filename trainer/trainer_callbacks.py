fea502d91d21fb5ac59889a7c9ff416310267bbb
Geremia Pompei <geremiapompei@gmail.com>
2022-10-06 08:46:09 +0200

    Bug fix trainer_callbacks

--- trainer/trainer_callbacks.py
+++ trainer/trainer_callbacks.py
@@ -1,6 +1,6 @@
 def print_epoch_loss_accuracy(train_set, test_set, zs_train_set=None, zs_test_set=None, accuracy=True):
     def print_loss_acc(name, loss, acc=None):
-        return f'\t{name}\t|\tloss:\t{loss:.3f}' + ('\t|\taccuracy:\t{acc:.3f}' if acc is not None else '')
+        return f'\t{name}\t|\tloss:\t{loss:.3f}' + (f'\t|\taccuracy:\t{acc:.3f}' if acc is not None else '')
 
     def callback(trainer, epoch, train_loss, test_loss, timer):
         epoch_mins, epoch_secs = timer
