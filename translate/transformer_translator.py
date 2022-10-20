531467797ce9d0b801354354181a8df0b8794e1a
Geremia Pompei <geremiapompei@gmail.com>
2022-10-20 12:06:42 +0200

    Bug fix on creation of translated set

--- translate/transformer_translator.py
+++ translate/transformer_translator.py
@@ -82,7 +82,8 @@ class TransformerTranslator:
             trg_tokens = self.tokenizer_decoder.tokenize(trg)
             pred = self.__call__(src)
             pred_tokens = self.tokenizer_decoder.tokenize(pred)
-            trg = self.tokenizer_decoder.convert_tokens_to_string(trg_tokens[1:-1])
+            trg_tokens = trg_tokens[1:-1]
+            trg = self.tokenizer_decoder.convert_tokens_to_string(trg_tokens)
             trgs.append([trg])
             preds.append(pred)
             trgs_tokens.append([trg_tokens])
